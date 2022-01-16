f6a1193b73c86b3d5e7015757ac6ff993892802f
mrodz <79176075+mrodz@users.noreply.github.com>
2022-01-15 19:39:20 -0800

    More updates + web content

--- bannerdeco.js
+++ bannerdeco.js
@@ -1,28 +1,33 @@
-var boxes = document.querySelectorAll('.content');
+const boxes = document.querySelectorAll('.box');
+const componentsToFadeIn = document.querySelectorAll('.offset-fade');
 const delay = millisFromSeconds(4.2);
+
+console.log(`There are ${boxes.length} boxes`);
 
 var vh = window.innerHeight / 100;
 var vw = window.innerWidth / 100;
 
-var paddingScale = 6;
-var topPadding = paddingScale * boxes.length;
-log(`topPadding = ${topPadding}`);
+var paddingScale = 3;
+var topPadding = paddingScale * componentsToFadeIn.length;
 
-boxes.forEach(box => {
+var opacity = 0.75
+var colors = [`rgba(${0xbf}, ${0x5d}, ${0x56}, ${opacity})`, `rgba(${0x5a}, ${0x9f}, ${0xa6}, ${opacity})`];
+
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].setAttribute('style', `border: solid ${colors[i]} 10px;${(i % 2 == 0 ? 'margin-top: 10vh;}' : '')}`);
+    console.log(`Setting box #${i} to color: ${colors[i]}`);
+}
+
+componentsToFadeIn.forEach(box => {
     box.classList.add('hidden')
     box.setAttribute('style', `padding-top: ${topPadding}vh;`);
-    log(`topPadding = ${topPadding}`);
     topPadding -= paddingScale;
 });
 
-function log(msg) {
-    console.log(msg);
-}
-
-setTimeout(function () {
-    for (let i = 0; i < boxes.length; i++) {
+setTimeout(() => {
+    for (let i = 0; i < componentsToFadeIn.length; i++) {
         setTimeout(() => {
-            boxes[i].classList.remove('hidden');
+            componentsToFadeIn[i].classList.remove('hidden');
         }, i * (delay / 10))
     }
 }, delay);
--- header.js
+++ header.js
@@ -2,15 +2,11 @@ const aboutMeItems = document.querySelectorAll('.about-items');
 const navBar = document.querySelector('.top-nav');
 var isActive = false;
 
-console.log(aboutMeItems);
-
 window.onscroll = function () { workHeader() };
 
 function workHeader() {
-    console.log(window.scrollY)
     if (window.scrollY > aboutMeItems[0].getBoundingClientRect().top && !isActive) {
         isActive = true;
-        // console.log(`${window.scrollY} > ${aboutMeItems[0].getBoundingClientRect().top}`);
         navBar.classList.add('visible-header', 'nav-fade-in');
         navBar.classList.remove('hidden-header', 'nav-fade-out');
     } else if (window.scrollY < aboutMeItems[0].getBoundingClientRect().top && isActive) {
